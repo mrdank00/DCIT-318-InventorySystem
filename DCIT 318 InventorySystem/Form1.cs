80ef71930682819a2a81d8e5fef4f80f67f96782
daniel kissi <kissidaniel7@gmail.com>
2022-08-21 15:01:04 +0000

    Add project files.

---
+++ DCIT 318 InventorySystem/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace DCIT_318_InventorySystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection con = invCRUD.invcon();
+            MySqlCommand cmd = new MySqlCommand("select * from users where userid='"+ textBox1.Text +"' and password='"+ textBox2.Text +"'", con);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da = new MySqlDataAdapter(cmd);
+            DataTable tbl = new DataTable();
+            da.Fill(tbl);
+            if (tbl.Rows.Count == 1)
+            {
+                this.Hide();
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Incorrect password");
+            }
+            con.Close();
+            System.Windows.Forms.MessageBox.Show("Sucess0");
+
+        }
+    }
+}
---
+++ DCIT 318 InventorySystem/invCRUD.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace DCIT_318_InventorySystem
+{
+    internal class invCRUD
+    {
+        public static MySqlConnection invcon()
+        {
+            string constr = "datasource=localhost;port=3306;username=root;password=;database=inventorysys";
+            MySqlConnection con = new MySqlConnection(constr);
+            try
+            {
+                con.Open();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Failed");
+                throw;
+            }
+            return con;
+            
+        }
+        public static void sql(string sqlcon)
+        {
+            MySqlConnection conn=invcon();
+            MySqlCommand cmd = new MySqlCommand(sqlcon,conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            System.Windows.Forms.MessageBox.Show("Sucess");
+
+        }
+        public static void display(string sqlcon,DataGridView dgv)
+        {
+            MySqlConnection conn = invcon();
+            MySqlCommand cmd = new MySqlCommand(sqlcon, conn);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da=new MySqlDataAdapter(cmd);
+            DataTable tbl = new DataTable();
+            da.Fill(tbl);
+            dgv.DataSource = tbl;
+            conn.Close();
+            System.Windows.Forms.MessageBox.Show("Sucess0");
+
+        }
+
+
+    }
+}
